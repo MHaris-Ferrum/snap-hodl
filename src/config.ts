2b7972d02b1907fc6b7e19934ffb9871371ecfc9
MHaris-Ferrum <muhammad.haris@ferrum.network>
2024-03-26 03:29:05 +0500

    trading volume fetching from pools

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // src/config.ts
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -8,26 +8,44 @@ export const APP_NAME = process.env.APP_NAME;
 export const DB_CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_COLLECTION = process.env.DB_COLLECTION;
-export const DB_COLLECTION_STAKING_SNAPSHOT = process.env.DB_COLLECTION_STAKING_SNAPSHOT;
-export const DB_COLLECTION_SNAP_CONFIG_BALANCE = process.env.DB_COLLECTION_SNAP_CONFIG_BALANCE;
-export const DB_COLLECTION_SNAP_HODL_CONFIG = process.env.DB_COLLECTION_SNAP_HODL_CONFIG;
+export const DB_COLLECTION_STAKING_SNAPSHOT =
+  process.env.DB_COLLECTION_STAKING_SNAPSHOT;
+export const DB_COLLECTION_SNAP_CONFIG_BALANCE =
+  process.env.DB_COLLECTION_SNAP_CONFIG_BALANCE;
+export const DB_COLLECTION_SNAP_HODL_CONFIG =
+  process.env.DB_COLLECTION_SNAP_HODL_CONFIG;
+export const DB_COLLECTION_TRADING_SNAP_HODL_CONFIG =
+  process.env.DB_COLLECTION_TRADING_SNAP_HODL_CONFIG;
+export const DB_COLLECTION_TRADING_SNAPSHOT =
+  process.env.DB_COLLECTION_TRADING_SNAPSHOT;
 export const CRON_SCHEDULE = process.env.CRON_SCHEDULE;
+export const VOLUME_CRON_SCHEDULE = process.env.VOLUME_CRON_SCHEDULE;
 export const PORT = process.env.PORT;
 
 // An array to hold the environment variables
 const envVariables = [
-  { name: 'APP_NAME', value: APP_NAME },
-  { name: 'DB_CONNECTION_STRING', value: DB_CONNECTION_STRING },
-  { name: 'DB_NAME', value: DB_NAME },
-  { name: 'DB_COLLECTION', value: DB_COLLECTION },
-  { name: 'DB_COLLECTION_STAKING_SNAPSHOT', value: DB_COLLECTION_STAKING_SNAPSHOT },
-  { name: 'DB_COLLECTION_SNAP_CONFIG_BALANCE', value: DB_COLLECTION_SNAP_CONFIG_BALANCE },
-  { name: 'CRON_SCHEDULE', value: CRON_SCHEDULE }
+  { name: "APP_NAME", value: APP_NAME },
+  { name: "DB_CONNECTION_STRING", value: DB_CONNECTION_STRING },
+  { name: "DB_NAME", value: DB_NAME },
+  { name: "DB_COLLECTION", value: DB_COLLECTION },
+  {
+    name: "DB_COLLECTION_STAKING_SNAPSHOT",
+    value: DB_COLLECTION_STAKING_SNAPSHOT,
+  },
+  {
+    name: "DB_COLLECTION_SNAP_CONFIG_BALANCE",
+    value: DB_COLLECTION_SNAP_CONFIG_BALANCE,
+  },
+  { name: "CRON_SCHEDULE", value: CRON_SCHEDULE },
+  { name: "VOLUME_CRON_SCHEDULE", value: VOLUME_CRON_SCHEDULE },
+  { name: "PORT", value: PORT },
 ];
 
 // Check if each environment variable is set
-envVariables.forEach(envVar => {
+envVariables.forEach((envVar) => {
   if (!envVar.value) {
-    throw new Error(`${envVar.name} is not set. Please set this environment variable.`);
+    throw new Error(
+      `${envVar.name} is not set. Please set this environment variable.`
+    );
   }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 // src/index.ts
-import { DB_CONNECTION_STRING, DB_NAME, PORT } from './config';
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import _ from 'lodash';
-import { scheduleJobs } from './cronJobs';
-import { getSnapHodlConfigs, createSnapHodlConfig, getSnapShotBySnapShotIdAndAddress, getAllSnapShots } from './controllers/snapHodlConfigController';
+import { DB_CONNECTION_STRING, DB_NAME, PORT } from "./config";
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import _ from "lodash";
+import { scheduleJobs } from "./cronJobs";
+import {
+  getSnapHodlConfigs,
+  createSnapHodlConfig,
+  getSnapShotBySnapShotIdAndAddress,
+  getAllSnapShots,
+} from "./controllers/snapHodlConfigController";
 
-import cors from 'cors';
+import cors from "cors";
 
 dotenv.config();
 const app = express();
@@ -17,29 +22,36 @@ app.use(express.json());
 // Enable all CORS requests
 app.use(cors());
 
-mongoose.connect(DB_CONNECTION_STRING as string, {
-  dbName: DB_NAME as string
-})
-  .then(() => console.log('MongoDB connection established'))
-  .catch(err => console.log('MongoDB connection error:', err));
+mongoose
+  .connect(DB_CONNECTION_STRING as string, {
+    dbName: DB_NAME as string,
+  })
+  .then(() => console.log("MongoDB connection established"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 scheduleJobs();
+// blockToBlockVolumeScheduleJobs();
 
-app.get('/', async (req, res) => {
-  res.send('Server running');
+app.get("/", async (req, res) => {
+  res.send("Server running");
 });
 
-app.get('/snapHodlConfig', getSnapHodlConfigs);
+app.get("/snapHodlConfig", getSnapHodlConfigs);
 
-app.post('/snapHodlConfig', createSnapHodlConfig);
+app.post("/snapHodlConfig", createSnapHodlConfig);
 
-app.get('/getSnapShotBySnapShotIdAndAddress/:snapShotId/:address', getSnapShotBySnapShotIdAndAddress);
-app.get('/getSnapShotBySnapShotIdAndAddress/:snapShotId/:address/raw', getSnapShotBySnapShotIdAndAddress);
+app.get(
+  "/getSnapShotBySnapShotIdAndAddress/:snapShotId/:address",
+  getSnapShotBySnapShotIdAndAddress
+);
+app.get(
+  "/getSnapShotBySnapShotIdAndAddress/:snapShotId/:address/raw",
+  getSnapShotBySnapShotIdAndAddress
+);
 
-app.get('/getAllSnapShots', getAllSnapShots);
+app.get("/getAllSnapShots", getAllSnapShots);
 
 // added snapshots
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
